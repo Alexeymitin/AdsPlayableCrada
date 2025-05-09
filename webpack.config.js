b5ee7123758063493e5ca3b18729cde451e33db0
Alexey Mitin <mitin.a@mascot.games>
2025-05-09 17:20:49 +0300

    update tsconfig.json, webpack.config.js and add global.d.ts

--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,91 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
-module.exports = {
-  entry: './src/index.ts',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true
-  },
-  resolve: {
-    extensions: ['.ts', '.js']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.(png|jpe?g|webp|gif)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'assets/[name][ext]'
-        }
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'public/index.html'
-    }),
-    new ImageMinimizerPlugin({
-      minimizer: {
-        implementation: ImageMinimizerPlugin.imageminGenerate,
-        options: {
-          plugins: [
-            ['mozjpeg', { quality: 75 }],
-            ['pngquant', { quality: [0.65, 0.9] }],
-            ['webp', { quality: 80 }]
-          ]
-        }
-      }
-    }),
-    new BundleAnalyzerPlugin({
-      analyzerMode: 'static'
-    })
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'dist')
+// const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+module.exports = (env, argv) => {
+  const isProd = argv.mode === 'production';
+
+  return {
+    entry: './src/index.ts',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+      publicPath: '/'
     },
-    compress: true,
-    port: 9000
-  },
-  devtool: 'source-map',
-  optimization: {
-    splitChunks: false,
-    usedExports: true,
-    minimize: true
-  },
-  performance: { hints: false }
+    resolve: {
+      extensions: ['.ts', '.js']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          use: {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true,
+              happyPackMode: true
+            }
+          },
+          exclude: /node_modules/
+        },
+        {
+          test: /\.(png|jpe?g|webp|gif)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'assets/[name][ext]'
+          }
+        },
+        {
+          test: /\.json$/,
+          type: 'asset/resource',
+          generator: {
+            filename: 'assets/[name][ext]'
+          }
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'public/index.html'
+      }),
+      ...(isProd
+        ? [
+            new ImageMinimizerPlugin({
+              minimizer: {
+                implementation: ImageMinimizerPlugin.imageminGenerate,
+                options: {
+                  plugins: [
+                    ['mozjpeg', { quality: 75 }],
+                    ['pngquant', { quality: [0.65, 0.9] }],
+                    ['webp', { quality: 80 }]
+                  ]
+                }
+              }
+            })
+          ]
+        : [])
+      // ...(isProd ? [new BundleAnalyzerPlugin()] : [])
+    ],
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'src')
+      },
+      compress: true,
+      port: 9000
+    },
+    devtool: isProd ? false : 'source-map',
+    optimization: {
+      splitChunks: false,
+      usedExports: true,
+      minimize: isProd
+    },
+    cache: {
+      type: 'filesystem'
+    },
+    performance: { hints: false },
+    mode: isProd ? 'production' : 'development'
+  };
 };
